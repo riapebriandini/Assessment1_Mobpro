434565acfbcb1c7d587e9c105ed6a2af6db6d651
Ria Pebrian Dini <riapebriandini98109@gmail.com>
2025-04-07 13:34:56 +0700

    Menambahkan input user, rumus hitung

--- app/src/main/java/com/riapebriandini607062330010/asesment1/MainActivity.kt
+++ app/src/main/java/com/riapebriandini607062330010/asesment1/MainActivity.kt
@@ -5,22 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.riapebriandini607062330010.asesment1.model.MakananSehat
+import com.riapebriandini607062330010.asesment1.ui.screen.MainScreen
 import com.riapebriandini607062330010.asesment1.ui.theme.Asesment1Theme
 
 class MainActivity : ComponentActivity() {
-    private val data = getData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,42 +21,13 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-    private fun getData(): MakananSehat {
-        return MakananSehat("Makanan Sehat", R.drawable.salad)
-    }
-}
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
+
+
+    @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+    @Composable
+    fun MainScreenPreview() {
+        Asesment1Theme {
+            MainScreen()
         }
-    ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
-    }
-}
-@Composable
-fun ScreenContent(modifier: Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
-}
-
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    Asesment1Theme {
-        MainScreen()
     }
 }
--- app/src/main/java/com/riapebriandini607062330010/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/riapebriandini607062330010/asesment1/ui/screen/MainScreen.kt
@@ -2,23 +2,48 @@ package com.riapebriandini607062330010.asesment1.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.GrammaticalInflectionManagerCompat.GrammaticalGender
 import com.riapebriandini607062330010.asesment1.R
 import com.riapebriandini607062330010.asesment1.model.MakananSehat
 import com.riapebriandini607062330010.asesment1.ui.theme.Asesment1Theme
@@ -44,9 +69,36 @@ fun MainScreen() {
 }
 @Composable
 fun ScreenContent(modifier: Modifier) {
+    var berat by remember { mutableStateOf("") }
+    var tinggi by remember { mutableStateOf("") }
+    var usia by remember { mutableStateOf("") }
+    var hasiKalori by remember { mutableStateOf("") }
+
+    val radioOptions = listOf(
+        stringResource(id = R.string.pria),
+        stringResource(id = R.string.wanita)
+    )
+    var gender by remember { mutableStateOf(radioOptions[0]) }
+
+    val aktivitasOptions = listOf(
+        "Sangat ringan (tidak olahraga)",
+        "Ringan (1–3x/minggu olahraga)",
+        "Sedang (3–5x/minggu olahraga)",
+        "Berat (6–7x/minggu olahraga)",
+        "Sangat berat (2x/hari olahraga)"
+    )
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedAktivitas by remember { mutableStateOf(aktivitasOptions[0]) }
+
+
     Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp) ,
         horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
         Image(
             painter = painterResource(id = R.drawable.salad),
@@ -58,7 +110,106 @@ fun ScreenContent(modifier: Modifier) {
             text = "Makanan Sehat untuk Tubuh Sehat!",
             style = MaterialTheme.typography.titleMedium
         )
+        OutlinedTextField(
+            value = berat,
+            onValueChange = { berat = it },
+            label = { Text(text = stringResource(R.string.berat)) },
+            trailingIcon = { Text(text = "kg") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(
+            value = tinggi,
+            onValueChange = { tinggi = it },
+            label = { Text(text = stringResource(R.string.tinggi)) },
+            trailingIcon = { Text(text = "cm") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+
+        OutlinedTextField(
+            value = usia,
+            onValueChange = { usia = it },
+            label = { Text(text = stringResource(R.string.usia)) },
+            trailingIcon = { Text(text = "tahun", modifier = Modifier.padding(end = 8.dp)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier.padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ) {
+            radioOptions.forEach { text ->
+                GenderOption(
+                    label = text,
+                    isSelected = gender == text,
+                    modifier = Modifier.selectable(
+                        selected = gender == text,
+                        onClick = { gender == text },
+                        role = Role.RadioButton
+                    )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+//        Drop
+//        ) { }
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.hitung))
+        }
     }
+}
+@Composable
+fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row (
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+fun hitungKalori(berat: Double, tinggi: Double, usia: Double, gender: String, aktivitas: String): Double {
+    val bmr = if (gender == "Pria") {
+        (10 * berat) + (6.25 * tinggi) - (5 * usia) + 5
+    } else {
+        (10 * berat) + (6.25 * tinggi) - (5 * usia) - 161
+    }
+
+    val faktorAktivitas = when (aktivitas) {
+        "Sangat ringan (tidak olahraga)" -> 1.2
+        "Ringan (1–3x/minggu olahraga)" -> 1.375
+        "Sedang (3–5x/minggu olahraga)" -> 1.55
+        "Berat (6–7x/minggu olahraga)" -> 1.725
+        "Sangat berat (2x/hari olahraga)" -> 1.9
+        else -> 1.2
+    }
+
+    return bmr * faktorAktivitas
 }
 
 
